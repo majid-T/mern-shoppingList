a2db16209936c92a004e634af2e6586326068935
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-01 15:05:18 +0430

    Routes for item GET/POST/DELETE done!

--- routes/apis/items.js
+++ routes/apis/items.js
@@ -14,11 +14,22 @@ router.get('/',(req,res,next)=>{
 });
 
 //@route    POST api/items
-//@desc     Create an Items
+//@desc     Create an Item
 //@access   Public
 router.post('/',(req,res,next)=>{
     const newItem = new Item({name: req.body.name});
     newItem.save().then((item)=>res.json(item));
 });
+
+
+//@route    DELETE api/items
+//@desc     Delete an Item
+//@access   Public
+router.delete('/:id',(req,res,next)=>{
+    Item.findById(req.params.id)
+    .then((item)=>item.remove().then(()=>res.json({success:true})))
+    .catch(err=>res.status(404).json({sucess:false}));
+});
+
 
 module.exports = router;
