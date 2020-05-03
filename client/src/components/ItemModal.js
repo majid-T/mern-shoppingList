41083cb2ac4a78d05ac382343429ff91ae6e63a1
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-03 14:01:35 +0430

    Added Modal Component

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import {
+    Button,Modal,ModalHeader,ModalBody,Form,FormGroup,Label,Input
+} from 'reactstrap';
+import {addItem} from '../actions/itemActions';
+import { connect } from 'react-redux';
+
+class ItemModal extends Component{
+    state={
+        modal:false,
+        name:''
+    }
+
+    toggle = ()=> {
+        this.setState({modal : !this.state.modal});
+    }
+
+    onChange = (e)=>{
+        this.setState({[e.target.name]:e.target.value});
+    }
+
+    render(){
+        return(
+            <div>
+                <Button color='dark' style={{marginBottom:'2rem'}} onClick={this.toggle}>
+                    Add Item
+                </Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>
+                        Add to ShoppingList
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.onSubmit}>
+                            <FormGroup>
+                                <Label for='item'>Item</Label>
+                                <Input type='text' name='name' id='item' placeholder='Add Shopping Item'
+                                onChange={this.onChange}/>
+                                <Button color='dark' style={{marginBottom:'2rem'}} block
+                                >Add Item</Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>                        
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default connect()(ItemModal);
