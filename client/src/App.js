e71b6ceb5f7263f02ed5909876ada4b398792f13
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-05 20:13:59 +0430

    added auth and error actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
-import {Provider} from 'react-redux';
-import store from './store';
-import ItemModal from './components/ItemModal';
-import {Container} from 'reactstrap';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import { Provider } from "react-redux";
+import store from "./store";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+import { loadUser } from "./actions/authActions";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar/>
-        <Container>
-          <ItemModal/>
-          <ShoppingList/>
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
-function auth (req,res,next){
-    const token = req.header('x-auth-token');
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
 
-    if(!token){
-        res.status(401).json({msg:'No Token, Authorization denied'});
-    }
+  if (!token) {
+    return res.status(401).json({ msg: "No Token, Authorization denied" });
+  }
 
-    try{
-        const decoded = jwt.verify(token,config.get('jwtSecret'));
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
 
-        req.user = decoded;
-        next();
-    }catch(ex){
-        res.status(400).json({msg:'Token is not valid'});
-    }
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
 }
 
 module.exports = auth;
